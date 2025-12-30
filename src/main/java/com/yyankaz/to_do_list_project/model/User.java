aabe98e4170bfb28703ad4aeb426fe1a726f05fe
yyankaz <yana.zavalniuk@stud.onu.edu.ua>
2025-12-30 13:14:57 +0200

    Security added. CustomUserDetailService class created.

--- src/main/java/com/yyankaz/to_do_list_project/model/User.java
+++ src/main/java/com/yyankaz/to_do_list_project/model/User.java
@@ -3,11 +3,13 @@ package com.yyankaz.to_do_list_project.model;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
+import lombok.Data;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
+@Data
 public class User {
     @Id
     private Long id;
---
+++ src/main/java/com/yyankaz/to_do_list_project/serviceImpl/CustomUserDetailService.java
@@ -0,0 +1,30 @@
+package com.yyankaz.to_do_list_project.serviceImpl;
+
+import com.yyankaz.to_do_list_project.model.User;
+import com.yyankaz.to_do_list_project.repository.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+
+@Service
+@AllArgsConstructor
+public class CustomUserDetailService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+        return org.springframework.security.core.userdetails.User
+                .withUsername(user.getUsername())
+                .password(user.getPassword())
+                .authorities(Collections.emptyList()) // без ролей
+                .build();
+    }
+}
